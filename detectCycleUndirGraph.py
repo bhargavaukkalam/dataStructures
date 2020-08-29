410a29568a0cdeb679ca50ff01fc9d0ac78bd47d
Bhargava Ukkalam <34873447+bhargavaukkalam@users.noreply.github.com>
2020-08-29 20:28:12 +0000

    Update detectCycleUndirGraph.py

--- detectCycleUndirGraph.py
+++ detectCycleUndirGraph.py
@@ -14,14 +14,21 @@ class Graph():
         print(self.graph)
         
     def isCyclic(self,n,graph):
+        '''
         visited=[False]*n
         for i in range(n):
             if not visited[i] and self.isCyclicConnected(n,graph,i,visited):
                 return True
         return False
+        '''
+        visited=[False]*len(graph)
+        for k,v in graph.items():
+            if not visited[k] and self.isCyclicConnected(n,graph,k,visited):
+                return True
+        return False
     
     def isCyclicConnected(self,n,graph,node,visited):
-        parent=[-1]*n
+        parent=[-1]*len(graph)
         queue=[]
         queue.append(node)
         visited[node]=True
@@ -29,6 +36,7 @@ class Graph():
         while queue:
             n=queue.pop(0)
             for v in graph[n]:
+                print(parent)
                 if not visited[v]:
                     visited[v]=True
                     queue.append(v)
